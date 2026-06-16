db8a8ac273ed774e57b4d000177e6d0cdff98914
pranyaaaaaa <pranyathakral84@gmail.com>
2026-06-16 22:05:36 +0530

    Add files via upload

---
+++ day12/q46.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isArmstrong(int n)
+{
+    int original = n, sum = 0, digits = 0;
+
+    int temp = n;
+    while (temp > 0)
+    {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = n;
+    while (temp > 0)
+    {
+        int digit = temp % 10;
+        sum += pow(digit, digits);
+        temp /= 10;
+    }
+
+    return sum == original;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isArmstrong(num))
+        cout << "Armstrong Number";
+    else
+        cout << "Not an Armstrong Number";
+
+    return 0;
+}
