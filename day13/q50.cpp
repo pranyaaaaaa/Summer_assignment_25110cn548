1acff0b9d96b4f17a3e21124bd9ee688f9ea7ab9
pranyaaaaaa <pranyathakral84@gmail.com>
2026-06-16 22:06:20 +0530

    Add files via upload

---
+++ day13/q50.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    double avg = (double)sum / n;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << avg;
+
+    return 0;
+}
