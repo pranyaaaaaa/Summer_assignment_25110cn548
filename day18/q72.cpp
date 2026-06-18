7223b63aac8ada2c20a6bd416b66775f23e7db8b
pranyaaaaaa <pranyathakral84@gmail.com>
2026-06-18 17:47:12 +0530

    Add files via upload

---
+++ day18/q72.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] < arr[j]) {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+
+    cout << "Array in descending order: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
